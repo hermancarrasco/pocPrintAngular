0fa11198e78fcd20fbae57d387b64e4beb34d00e
Herman Carrasco <hcarrasco@smartjob.cl>
2020-01-28 12:50:56 -0300

    printer angular google chrome

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { ThermalPrintModule } from 'ng-thermal-print';
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -11,6 +11,7 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ThermalPrintModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { PrintService, UsbDriver, WebPrintDriver } from 'ng-thermal-print';
+import { PrintDriver } from 'ng-thermal-print/lib/drivers/PrintDriver';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+    status = false;
+    usbPrintDriver: UsbDriver;
+    webPrintDriver: WebPrintDriver;
+    ip = '';
+    constructor(private printService: PrintService) {
+        this.usbPrintDriver = new UsbDriver();
+        this.printService.isConnected.subscribe(result => {
+            this.status = result;
+            if (result) {
+                console.log('Connected to printer!!!');
+            } else {
+                console.log('Not connected to printer.');
+            }
+        });
+    }
+    ngOnInit(): void {
+        this.requestUsb();
+    }
 
-  constructor() {}
+
+    requestUsb() {
+        this.usbPrintDriver.requestUsb().subscribe(result => {
+            console.log('result usb:', result);
+            this.printService.setDriver(this.usbPrintDriver, 'ESC/POS');
+        });
+    }
+
+    connectToWebPrint() {
+        this.webPrintDriver = new WebPrintDriver(this.ip);
+        console.log('connect ip:', this.webPrintDriver);
+        this.printService.setDriver(this.webPrintDriver, 'WebPRNT');
+    }
+
+    print(driver: PrintDriver) {
+        console.log('driver: ', driver);
+        this.printService.init()
+            .setBold(true)
+            .setJustification('center')
+            .setSize('large')
+            .setUnderline(true)
+            .writeLine('A01')
+            .setUnderline(false)
+            .setSize('large')
+            .writeLine('A02')
+            .setBold(false)
+            .feed(4)
+            .cut('full')
+            .flush();
+    }
 
 }
